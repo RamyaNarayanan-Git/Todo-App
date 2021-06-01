06800e96be3a4bf3da2b472892f1049481dd2457
Ramya <ramyanaryn@gmail.com>
2021-06-01 16:13:52 +0800

    initial commit

--- functions/util/config.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-    apiKey: "AIzaSyBuXOwfoItLfrn6FbJ9-GGzbbdOb-HXuco",
-    authDomain: "todoapp-82a79.firebaseapp.com",
-    projectId: "todoapp-82a79",
-    storageBucket: "todoapp-82a79.appspot.com",
-    messagingSenderId: "1039148789644",
-    appId: "1:1039148789644:web:69ce7ec9a3041fe984b0a9"
-  };
